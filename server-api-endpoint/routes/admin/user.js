90fc07af49d1367c7606f4d13bf9c6be09c71fbb
PrawiraGenestonlia <prawira.96@gmail.com>
2019-10-29 10:44:45 +0000

    delete user api

--- server-api-endpoint/routes/admin/user.js
+++ server-api-endpoint/routes/admin/user.js
@@ -16,14 +16,22 @@ router.get('/get-all-user', verifyToken, async (req, res) => {
     allUsersArr[i].action = 'action';
     allUsersArr[i].role = { options: ['superadmin', 'clubadmin', 'student', 'mentor'], current: allUsersArr[i].role };
   }
-
   let response = {
     columns: ['name', 'email', 'role', 'action'],
     data: allUsersArr
   }
-
   res.status(200).json(response);
 })
 
+router.post('/delete-user', verifyToken, async (req, res) => {
+  if (req.user.role !== "superadmin") return res.status(401).send('Unauthorized Access!');
+  try {
+    let allUsers = await user.deleteOne({ _id: req.body._id });
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+})
 
 module.exports = router;
