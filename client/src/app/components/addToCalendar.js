c7b5d383c8b5a31725fdb9834aaf540b73f579cf
PrawiraGenestonlia <prawira.96@gmail.com>
2019-11-02 03:06:19 +0800

    successful migration

--- client/src/app/components/addToCalendar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AddToCalendar from 'react-add-to-calendar';
-import './node_modules/react-add-to-calendar/dist/react-add-to-calendar.css';
-
-export default (props) => (
-  <div className={`${props.className} ${props.class}`}>
-    <AddToCalendar event={props.event} buttonLabel="Create calendar event" buttonTemplate={{ 'calendar-plus-o': 'left' }} />
-  </div>
-)
