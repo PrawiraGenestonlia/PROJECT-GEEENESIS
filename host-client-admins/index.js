37f5fe5639361d8d43db746684e9e23c8c6d2558
PrawiraGenestonlia <prawira.96@gmail.com>
2019-11-01 08:38:41 +0000

    serve client admins

---
+++ host-client-admins/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.disable('x-powered-by');
+
+app.use(express.static(path.join(__dirname, '../client-admins/build')));
+
+console.log(path.join(__dirname, '../client-admins/build'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client-admins/build', "/index.html"));
+});
+
+app.listen(
+  process.env.PORT || 3840,
+  function () {
+    console.log(`geeenesis-admin-webapp start on http://localhost:3840`)
+  }
+);
