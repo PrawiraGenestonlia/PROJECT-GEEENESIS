2e650b6d1e30cd182473a6b6acb7bf5564aa3553
PrawiraGenestonlia <prawira.96@gmail.com>
2019-11-04 01:44:03 +0800

    test reload

--- client/src/components/forceReloadBar.js
+++ client/src/components/forceReloadBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 export default () => (
-  <div id="reloadbar" onClick={() => { window.location.reload(true) }}>
+  <div id="reloadbar" className="show" onClick={async () => {
+    await navigator.serviceWorker.getRegistrations().then(registrations => {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+    window.location.reload(true);
+  }}>
     A new version of this app is available. Click <span><strong>here</strong></span> to update.
   </div>
 )
