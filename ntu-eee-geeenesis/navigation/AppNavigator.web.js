7eebd2eb1705703d75b6166e4af2411b0fb624e7
PrawiraGenestonlia <prawira.96@gmail.com>
2019-12-27 15:50:48 +0800

    added expo

---
+++ ntu-eee-geeenesis/navigation/AppNavigator.web.js
@@ -0,0 +1,22 @@
+import { createBrowserApp } from '@react-navigation/web';
+import { createSwitchNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
+import MainTabNavigator from './MainTabNavigator';
+
+const switchNavigator = createSwitchNavigator({
+  // You could add another route here for authentication.
+  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Main: MainTabNavigator,
+});
+switchNavigator.path = '';
+
+if (Platform.OS === 'web' && document) {
+  const rnwStyleSheet = document.getElementById('react-native-stylesheet')
+  if (rnwStyleSheet) {
+    rnwStyleSheet.sheet.insertRule(`html {
+      overflow: hidden;
+    }`, 0);
+  }
+}
+
+export default createBrowserApp(switchNavigator, { history: 'hash' });
