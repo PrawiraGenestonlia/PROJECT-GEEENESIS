f4396d623729b56e3e7e1beccd2d24c102911035
PrawiraGenestonlia <prawira.96@gmail.com>
2020-02-12 18:29:57 +0800

    Fast Sync

--- mobile-pwa-nextjs/next.config.js
+++ mobile-pwa-nextjs/next.config.js
@@ -27,7 +27,7 @@ module.exports = withCSS(withPurgeCSS(withOffline({
     }
     return config
   },
-  registerSwPrefix: `${process.env.BASE_PATH ? process.env.BASE_PATH + '/_next/static/' : ''}`,
+  registerSwPrefix: `${process.env.BASE_PATH ? process.env.BASE_PATH + '/_next/static' : ''}`,
   scope: process.env.BASE_PATH || '',
   workboxOpts: {
     swDest: process.env.NEXT_EXPORT
