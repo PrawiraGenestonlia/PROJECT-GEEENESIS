ac6bb9db04f4392881219de9e85ea8f35164f814
PrawiraGenestonlia <prawira.96@gmail.com>
2020-02-12 22:07:55 +0800

    Fast Sync

--- mobile-pwa-nextjs/next.config.js
+++ mobile-pwa-nextjs/next.config.js
@@ -47,15 +47,15 @@ module.exports = withCSS(withPurgeCSS(withOffline({
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        // source: `${process.env.BASE_PATH ? process.env.BASE_PATH : ''}/service-worker.js`,
-        source: `^/service-worker.js$`,
-        destination: `${process.env.BASE_PATH ? process.env.BASE_PATH : ''}/_next/static/service-worker.js`,
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       // source: `${process.env.BASE_PATH ? process.env.BASE_PATH : ''}/service-worker.js`,
+  //       source: `^/service-worker.js$`,
+  //       destination: `${process.env.BASE_PATH ? process.env.BASE_PATH : ''}/_next/static/service-worker.js`,
+  //     },
+  //   ]
+  // },
   // experimental: {
   //   async rewrites() {
   //     return [
