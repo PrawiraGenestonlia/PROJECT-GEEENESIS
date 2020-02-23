1c15e3e1c5f6b0b11b34326dc1903fa1639ab061
PrawiraGenestonlia <prawira.96@gmail.com>
2020-02-24 04:09:59 +0800

    Fast Sync

--- server-api-endpoint/routes/profile/profile.js
+++ server-api-endpoint/routes/profile/profile.js
@@ -199,7 +199,7 @@ router.get('/get-my-chat-list', verifyToken, async (req, res) => {
 
     let response = [];
 
-    for (let i = 0; i < chatList.size; i++) {
+    for (let i = 0; i < chatList.size(); i++) {
       let doc = await user.findOne({ networkname: chatList[i] });
       console.log(doc);
       response.push({ networkname: chatList[i], name: doc._doc['name'] });
