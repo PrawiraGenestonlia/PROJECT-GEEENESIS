82b4771e165367d1dbc9d93faf302f8908e26b75
prawiragenestonlia <prawira.96@gmail.com>
2020-05-19 18:00:40 +0800

    Fast Sync

--- server-api-endpoint/routes/chat/chat.js
+++ server-api-endpoint/routes/chat/chat.js
@@ -21,6 +21,7 @@ router.post('/get-chats', verifyToken, async (req, res) => {
         let chatLists = [];
         for (let i = 0; i < foundChats.length; i++) {
             let cChat = { ...foundChats[i]._doc };
+            cChat.time = new Date(cChat).toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
             if (foundChats[i].senderName == myNetworkName) {
                 cChat.id = 0;
             } else {
@@ -43,7 +44,7 @@ router.post('/post-chats', verifyToken, async (req, res) => {
         senderName: myNetworkName,
         receiverName: req.body.receiverName,
         message: req.body.message,
-        time: req.body.time ? new Date(req.body.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore' }) : new Date().toLocaleString('en-US', { timeZone: 'Asia/Singapore' }),
+        time: req.body.time ? new Date(req.body.time).toISOString() : new Date().toISOString(),
     });
     try {
         //
@@ -53,6 +54,7 @@ router.post('/post-chats', verifyToken, async (req, res) => {
         let chatLists = [];
         for (let i = 0; i < foundChats.length; i++) {
             let cChat = { ...foundChats[i]._doc };
+            cChat.time = new Date(cChat).toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
             if (foundChats[i].senderName == myNetworkName) {
                 cChat.id = 0;
             } else {
